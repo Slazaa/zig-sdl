7a49db4c5fe5c2c1a8e8e458a77f1c8dde7f3836
Slazaa <slazaa42@gmail.com>
2024-01-10 12:31:23 +0100

    Linux support

--- build.zig
+++ build.zig
@@ -7,7 +7,7 @@ inline fn thisDir() []const u8 {
     return comptime std.fs.path.dirname(@src().file) orelse ".";
 }
 
-pub fn link(b: *std.Build, exe: *std.Build.LibExeObjStep, linkage: std.Build.LibExeObjStep.Linkage) void {
+pub fn link(b: *std.Build, exe: *std.Build.Step.Compile, linkage: std.Build.Step.Compile.Linkage) void {
     exe.linkLibC();
 
     b.installBinFile(sdl_path ++ "/bin/SDL2.dll", "SDL2.dll");
@@ -16,11 +16,6 @@ pub fn link(b: *std.Build, exe: *std.Build.LibExeObjStep, linkage: std.Build.Lib
 
     exe.addIncludePath(.{ .path = sdl_path ++ "/include" });
     exe.addLibraryPath(.{ .path = sdl_path ++ "/lib" });
-
-    exe.linkSystemLibrary("sdl2");
-    exe.linkSystemLibrary("sdl2_image");
-
-    exe.linkSystemLibrary("sdl2_ttf");
 
     if (builtin.target.isDarwin()) {
         exe.linkSystemLibrary("freetype");
@@ -71,20 +66,24 @@ pub fn link(b: *std.Build, exe: *std.Build.LibExeObjStep, linkage: std.Build.Lib
                 exe.addObjectFile(.{ .path = sdl_path ++ "/lib/libSDL2_ttf.dll.a" });
             },
         }
+    } else if (builtin.os.tag == .linux) {
+        exe.linkSystemLibrary("sdl2");
+        exe.linkSystemLibrary("sdl2_image");
+        exe.linkSystemLibrary("sdl2_ttf");
     }
 
     const lib = switch (linkage) {
         .static => b.addStaticLibrary(.{
             .name = "sdl2",
             .root_source_file = .{ .path = thisDir() ++ "/src/sdl.zig" },
-            .target = exe.target,
-            .optimize = exe.optimize,
+            .target = exe.root_module.resolved_target.?,
+            .optimize = exe.root_module.optimize.?,
         }),
         .dynamic => b.addSharedLibrary(.{
             .name = "sdl2",
             .root_source_file = .{ .path = thisDir() ++ "/src/sdl.zig" },
-            .target = exe.target,
-            .optimize = exe.optimize,
+            .target = exe.root_module.resolved_target.?,
+            .optimize = exe.root_module.optimize.?,
         }),
     };
 
@@ -95,4 +94,24 @@ pub fn getModule(b: *std.Build) *std.Build.Module {
     return b.createModule(.{
         .source_file = .{ .path = thisDir() ++ "/src/sdl.zig" },
     });
+}
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const basic_example = b.addExecutable(.{
+        .name = "basic",
+        .root_source_file = .{ .path = "example/basic.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    link(b, basic_example, .static);
+
+    const run_cmd = b.addRunArtifact(basic_example);
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    const run_step = b.step("example-basic", "Run the basic example");
+    run_step.dependOn(&run_cmd.step);
 }
